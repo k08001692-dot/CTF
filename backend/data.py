17c62bcae3a56eea9454488d13ec620f40e15092
k08001692-dot <k08001692@gmail.com>
2025-12-26 12:20:43 +0530

    Add files via upload

---
+++ backend/data.py
@@ -0,0 +1,28 @@
+users = [
+    {
+        "email": "user@bookhaven.com",
+        "password": "password123"
+    },
+    {
+        "email": "admin@bookhaven.com",
+        "password": "admin123"
+    }
+]
+
+books = [
+    {
+        "id": 1,
+        "title": "The Silent Algorithm",
+        "price": 499
+    },
+    {
+        "id": 2,
+        "title": "Pastel Secrets",
+        "price": 399
+    },
+    {
+        "id": 3,
+        "title": "Hidden Pages",
+        "price": 299
+    }
+]
